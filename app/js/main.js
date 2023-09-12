6cb03acf451b8ee80aa4eece277145f8a94449cf
Olha Sashchuk <olha.sashchuk@gmail.com>
2023-09-12 23:29:12 +0200

    change index page

--- app/js/main.js
+++ app/js/main.js
@@ -31,14 +31,67 @@
    })
 })();
 
-const teamSwiper = new Swiper('.team__swiper', {
-   spaceBetween: 30,
-   autoHeight: true,
-   loop: true,
-   autoplay: true,
-   pagination: {
-     el: '.team__swiper-pagination',
-   },
- });
+// slider team
+const isTeamSlider = document.querySelector('.team__swiper');
+if(isTeamSlider) {
+   const teamSwiper = new Swiper('.team__swiper', {
+      spaceBetween: 30,
+      autoHeight: true,
+      loop: true,
+      autoplay: true,
+      pagination: {
+      el: '.team__swiper-pagination',
+      }
+   })
+};
+
+// slider achievment
+const isAchievmentSlider = document.querySelector('.achievment__swiper');
+if(isAchievmentSlider) {
+   const achievmentSwiper = new Swiper('.achievment__swiper', {
+      slidesPerView: 1,
+      autoHeight: true,
+      loop: true,
+      autoplay: true,
+      pagination: {
+      el: '.achievment__swiper-pagination',
+      },
+      breakpoints: {
+         768: {
+           slidesPerView: 4,
+           spaceBetween: 40
+         }
+      }
+   })
+};
+
+// pop up form
+(() => {
+   const popUpForm = document.querySelector('.form');
+   const openFormBtn = document.querySelector('.ecommerce__body-btn');
+   const closeFormBtn = document.querySelector('.form__close');
+   const isForm = document.querySelector('.form__content');
+   const submitFormBtn = document.querySelector('.form__btn');
+
+   openFormBtn.addEventListener('click', openForm);
+   closeFormBtn.addEventListener('click', closeForm);
+   isForm.addEventListener('submit', onSubmitForm);
+
+   function closeForm(){
+      popUpForm.classList.remove('active');
+   };
+   function openForm(){
+      popUpForm.classList.add('active');
+   };
+
+   function onSubmitForm(e){
+      e.preventDefault();
+
+      setTimeout(()=>{
+            popUpForm();
+         submitFormBtn.classList.add('active');
+      }, 800)
+   }
+})();
 
 
